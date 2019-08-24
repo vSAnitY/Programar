d56b30e60fe7c81193a01f4f986d71b8dab56044
roberto solis <robertosantos201109@hotmail.com>
2019-08-23 22:37:56 -0500

    Actividad 2

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Iniciar = new System.Windows.Forms.Button();
+            this.Salir = new System.Windows.Forms.Button();
+            this.Usuario = new System.Windows.Forms.Label();
+            this.Contraseña = new System.Windows.Forms.Label();
+            this.c = new System.Windows.Forms.TextBox();
+            this.u = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Iniciar
+            // 
+            this.Iniciar.Location = new System.Drawing.Point(271, 337);
+            this.Iniciar.Name = "Iniciar";
+            this.Iniciar.Size = new System.Drawing.Size(75, 23);
+            this.Iniciar.TabIndex = 0;
+            this.Iniciar.Text = "Iniciar";
+            this.Iniciar.UseVisualStyleBackColor = true;
+            this.Iniciar.Click += new System.EventHandler(this.Iniciar_Click);
+            // 
+            // Salir
+            // 
+            this.Salir.Location = new System.Drawing.Point(417, 337);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(75, 23);
+            this.Salir.TabIndex = 1;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            this.Salir.Click += new System.EventHandler(this.Salir_Click);
+            // 
+            // Usuario
+            // 
+            this.Usuario.AutoSize = true;
+            this.Usuario.BackColor = System.Drawing.Color.Black;
+            this.Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Usuario.ForeColor = System.Drawing.Color.White;
+            this.Usuario.Location = new System.Drawing.Point(198, 158);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(86, 25);
+            this.Usuario.TabIndex = 2;
+            this.Usuario.Text = "Usuario";
+            this.Usuario.Click += new System.EventHandler(this.Usuario_Click);
+            // 
+            // Contraseña
+            // 
+            this.Contraseña.AutoSize = true;
+            this.Contraseña.BackColor = System.Drawing.Color.Black;
+            this.Contraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Contraseña.ForeColor = System.Drawing.Color.White;
+            this.Contraseña.Location = new System.Drawing.Point(170, 211);
+            this.Contraseña.Name = "Contraseña";
+            this.Contraseña.Size = new System.Drawing.Size(123, 25);
+            this.Contraseña.TabIndex = 3;
+            this.Contraseña.Text = "Contraseña";
+            // 
+            // c
+            // 
+            this.c.Location = new System.Drawing.Point(306, 216);
+            this.c.Name = "c";
+            this.c.PasswordChar = '*';
+            this.c.Size = new System.Drawing.Size(134, 20);
+            this.c.TabIndex = 4;
+            this.c.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // u
+            // 
+            this.u.Location = new System.Drawing.Point(306, 158);
+            this.u.Name = "u";
+            this.u.Size = new System.Drawing.Size(134, 20);
+            this.u.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(229, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 37);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Bienvenido";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.eaaa;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.u);
+            this.Controls.Add(this.c);
+            this.Controls.Add(this.Contraseña);
+            this.Controls.Add(this.Usuario);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.Iniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Iniciar;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.Label Usuario;
+        private System.Windows.Forms.Label Contraseña;
+        private System.Windows.Forms.TextBox c;
+        private System.Windows.Forms.TextBox u;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Usuario_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Iniciar_Click(object sender, EventArgs e)
+        {
+            if (u.Text=="juancho"&&c.Text=="123tamarindo")
+            {
+                MessageBox.Show("Se ha iniciado sesion");
+            }
+            else
+            {
+                MessageBox.Show("Inicio de sesion fallido");
+                u.Text = "";
+                c.Text = "";
+            }
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
