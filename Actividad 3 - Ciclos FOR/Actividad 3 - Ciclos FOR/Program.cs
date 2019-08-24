1a970d3b44e9ffa7f33fe303ed7b6bca01b62abc
roberto solis <robertosantos201109@hotmail.com>
2019-08-24 16:47:17 -0500

    Actividad 2

---
+++ Actividad 3 - Ciclos FOR/Actividad 3 - Ciclos FOR/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Tarea_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Desde que numero quieres empezar: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hasta donde va a terminar el ciclo el ciclo: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De cuanto en cuanto quieres ir? ");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            if (x < y)
+            {
+                for (int i = x; i <= y; i += z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+            if (x > y)
+            {
+                for (int i = x; i >= y; i -= z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+        }
+    }
+}
